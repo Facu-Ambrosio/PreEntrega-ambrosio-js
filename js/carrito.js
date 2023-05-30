c95daba6bbe90bf1a3cb94641ed25af9b1870fef
Facundo Nahuel Ambrosio <facu.n.ambrosio@gmail.com>
2023-05-30 15:28:30 -0300

    esta maquetado todo el catalogo y el carrito, se pueden agregar elementos al carrito apretando el boton de agregar carrito en el catalogo, en la pagina de carrito se pueden quitar, faltaria depurar mas el codigo js, y corregir el error de que se borra todos los elementoscuando se apreta dos veces en el boton de sacar de carrito

---
+++ js/carrito.js
@@ -0,0 +1,49 @@
+const armadoCarrito = (carrito, mainCarrito) => {
+      for (let dibujo of carrito) {
+            let fila = document.createElement("section");
+            fila.classList.add("container", "sectionCarrito");
+            fila.innerHTML = `
+            <div class="row align-items-center">
+                  <div class="col-1">
+                        <img class="imagenCarrito" src="../Assets/Galeria/reescaladas/${dibujo.nombre}.jpg" alt="">
+                  </div>
+                  <div class="col-7">
+                        ${dibujo.nombre} (x ${dibujo.cantidad})
+                  </div>
+                  <div class="col-2">
+                        precio:  $${dibujo.precio * dibujo.cantidad}
+                  </div>
+                  <div class="col-2">
+                        <button type="button" class="btn btn-danger" id = "${dibujo.nombre}">Sacar del carrito</button>
+                  </div>
+            </div>
+            `;
+            mainCarrito.appendChild(fila);
+      }
+}
+
+const sacarCarrito = (e) => {
+      let nombre = e.target.id;
+      let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+      localStorage.clear();
+      let elementoASacar = carritoStorage.find((e) => e.nombre === nombre).nombre; //objeto a sacar 
+      let carritoNuevo = carritoStorage.filter((el) => el.nombre !== elementoASacar);
+      localStorage.setItem("carrito", JSON.stringify(carritoNuevo));
+      alert(`Acaba de sacar el/los dibujo/s "${nombre}" del carrito, recargue la pagina`)
+}
+// -------------------------------------------
+let mainCarrito = document.getElementById("mainCarrito");
+
+let carrito = JSON.parse(localStorage.getItem("carrito"));
+
+let sacar = document.getElementsByTagName("button");
+
+armadoCarrito(carrito, mainCarrito); //funcion que arma la estructura HTML del carrito
+
+for (let botones of sacar) {
+      botones.addEventListener("click", sacarCarrito);
+}
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,46 @@
+const productos = [
+      { nombre: "15 Dias Mas", precio: 9000, cantidad: 1 },
+      { nombre: "Muerte Roja", precio: 15000, cantidad: 1 },
+      { nombre: "Demonios Internos", precio: 9000, cantidad: 1 },
+      { nombre: "Vampire Batman", precio: 15000, cantidad: 1 },
+      { nombre: "The Hand", precio: 9000, cantidad: 1 },
+      { nombre: "Pizza y Alcohol en Gel", precio: 15000, cantidad: 1 },
+      { nombre: "Principe Prospero", precio: 9000, cantidad: 1 },
+      { nombre: "The Old Men", precio: 5000, cantidad: 1 },
+]
+const agregarCarrito = (e) => {
+      let dibujo = e.target.id; //id del dibujo
+      let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+      if (carritoStorage == null) {
+            let carrito = [];
+            carrito.push(productos.find((el) => el.nombre === dibujo));
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            alert(`Usted agrego ${carrito.find((el) => el.nombre === dibujo).cantidad} dibujo/s  de "${dibujo}" a su carrito`)
+      } else if (carritoStorage.some((el) => el.nombre === dibujo) === false) {
+            carritoStorage.push(productos.find((el) => el.nombre === dibujo));
+            localStorage.setItem("carrito", JSON.stringify(carritoStorage));
+            alert(`Usted agrego ${carritoStorage.find((el) => el.nombre === dibujo).cantidad} dibujo/s  de "${dibujo}" a su carrito`)
+      } else {
+            carritoStorage.find((el) => el.nombre === dibujo).cantidad += 1;
+            localStorage.setItem("carrito", JSON.stringify(carritoStorage));
+            alert(`Usted agrego ${carritoStorage.find((el) => el.nombre === dibujo).cantidad} dibujo/s  de "${dibujo}" a su carrito`)
+      }
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+let mainCatalogo = document.getElementById("mainCatalogo");
+
+let boton = mainCatalogo.querySelectorAll("button");
+
+for (let i of boton) {
+      i.addEventListener("click", agregarCarrito);
+}
+
+
+
+
+
+
+
