988cea5d01c8efbec78226266d990b087b3acd94
Facundo Nahuel Ambrosio <facu.n.ambrosio@gmail.com>
2023-05-30 22:31:20 -0300

    depure las funciones de los dos JS, arregle un problema de la funcion sacarCarrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,8 +1,8 @@
-const armadoCarrito = (carrito, mainCarrito) => {
-      for (let dibujo of carrito) {
-            let fila = document.createElement("section");
-            fila.classList.add("container", "sectionCarrito");
-            fila.innerHTML = `
+const armadoCarrito = () => { //funcion que arma la estrucutra html del carrito en el carrito.html
+      for (let dibujo of carrito) { //por cada articulo en el carrito del storage
+            let fila = document.createElement("section"); //crea una etiqueta section
+            fila.classList.add("container", "sectionCarrito"); //a dicha etiquete section le agrega las clases container y sectionCarrito
+            fila.innerHTML = ` 
             <div class="row align-items-center">
                   <div class="col-1">
                         <img class="imagenCarrito" src="../Assets/Galeria/reescaladas/${dibujo.nombre}.jpg" alt="">
@@ -17,30 +17,33 @@ const armadoCarrito = (carrito, mainCarrito) => {
                         <button type="button" class="btn btn-danger" id = "${dibujo.nombre}">Sacar del carrito</button>
                   </div>
             </div>
-            `;
-            mainCarrito.appendChild(fila);
+            `; //estructura HTML del carrito
+            mainCarrito.appendChild(fila); //se agrega el section nuevo al main de carrito.html
       }
 }
-const sacarCarrito = (e) => {
-      let nombre = e.target.id;
-      let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
-      localStorage.clear();
-      let elementoASacar = carritoStorage.find((e) => e.nombre === nombre).nombre; //objeto a sacar 
-      let carritoNuevo = carritoStorage.filter((el) => el.nombre !== elementoASacar);
-      localStorage.setItem("carrito", JSON.stringify(carritoNuevo));
-      alert(`Acaba de sacar el/los dibujo/s "${nombre}" del carrito, recargue la pagina`)
+const sacarCarrito = (e) => { //funcion que permite sacar articulos del carrito
+      let nombre = e.target.id; //se declara una variable que almacena el id del boton que coincide con el nombre del articulo
+      let carritoStorage = JSON.parse(localStorage.getItem("carrito")); // se obtiene del storage el carrito
+      let elementoASacar = carritoStorage.find((e) => e.nombre === nombre); //objeto a sacar 
+      if (elementoASacar) { //si el elemento que se quiere sacar  no se ha sacado antes
+            let carritoNuevo = carritoStorage.filter((el) => el.nombre !== nombre); //se crea un carrito nuevo sin incluir el articulo
+            localStorage.setItem("carrito", JSON.stringify(carritoNuevo)); //se sube al storage el carrito nuevo
+            alert(`Acaba de sacar el/los dibujo/s "${nombre}" del carrito. Recargue la página.`); //aviso al usuario de que saco el articulo del carrito, una vez que se recargue la pagina el articulo desaparecera
+      } else { //en caso de que el usuario no recargue la pagina una vez eliminado el articulo, y se vuelva a apretar el boton de sacare del carrito
+            alert(`El dibujo "${nombre}" ya se quitó del carrito, recargue la pagina`); //aviso de que ya lo ha sacado del carrito
+      }
 }
 // -------------------------------------------
-let mainCarrito = document.getElementById("mainCarrito");
+let mainCarrito = document.getElementById("mainCarrito"); //main del carrito.html
 
-let carrito = JSON.parse(localStorage.getItem("carrito"));
+let carrito = JSON.parse(localStorage.getItem("carrito")); //carrito del storage
 
-let sacar = document.getElementsByTagName("button");
+let sacar = document.getElementsByTagName("button"); //botones para sacar elementos del carrito
 
 armadoCarrito(carrito, mainCarrito); //funcion que arma la estructura HTML del carrito
 
-for (let botones of sacar) {
-      botones.addEventListener("click", sacarCarrito);
+for (let botones of sacar) { //por cada boton de sacar del carrito en el carrito.html se declara un evento
+      botones.addEventListener("click", sacarCarrito); //evento del tipo click y que acciona la funcion sacarCarrito
 }
 
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const productos = [
+const productos = [ //array con todos los productos disponibles
       { nombre: "15 Dias Mas", precio: 9000, cantidad: 1 },
       { nombre: "Muerte Roja", precio: 15000, cantidad: 1 },
       { nombre: "Demonios Internos", precio: 9000, cantidad: 1 },
@@ -8,30 +8,31 @@ const productos = [
       { nombre: "Principe Prospero", precio: 9000, cantidad: 1 },
       { nombre: "The Old Men", precio: 5000, cantidad: 1 },
 ]
-const agregarCarrito = (e) => {
-      let dibujo = e.target.id; //id del dibujo
-      let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
-      if (carritoStorage == null) {
-            let carrito = [];
-            carrito.push(productos.find((el) => el.nombre === dibujo));
-            localStorage.setItem("carrito", JSON.stringify(carrito));
-            alert(`Usted agrego ${carrito.find((el) => el.nombre === dibujo).cantidad} dibujo/s  de "${dibujo}" a su carrito`)
-      } else if (carritoStorage.some((el) => el.nombre === dibujo) === false) {
-            carritoStorage.push(productos.find((el) => el.nombre === dibujo));
-            localStorage.setItem("carrito", JSON.stringify(carritoStorage));
-            alert(`Usted agrego ${carritoStorage.find((el) => el.nombre === dibujo).cantidad} dibujo/s  de "${dibujo}" a su carrito`)
-      } else {
-            carritoStorage.find((el) => el.nombre === dibujo).cantidad += 1;
-            localStorage.setItem("carrito", JSON.stringify(carritoStorage));
-            alert(`Usted agrego ${carritoStorage.find((el) => el.nombre === dibujo).cantidad} dibujo/s  de "${dibujo}" a su carrito`)
+const agregarCarrito = (e) => { //funcion que agrega al carrito los productos
+      let  dibujo = e.target.id; // id del dibujo
+      let carritoStorage = JSON.parse(localStorage.getItem("carrito")) || []; // Si no hay elementos en el carrito del localStorage, se asigna un array vacío
+
+      let  dibujoExistente = carritoStorage.find((el) => el.nombre === dibujo); //variable que  indica si el dibujo al cual se agrega al carrito existe o no
+
+      if (dibujoExistente) { //en caso de que el dibujo que se quiere agregar al carrito ya estuviera en este
+            dibujoExistente.cantidad += 1; //se le suma 1 a la cantidad en el carrito
+            alert(`Usted agregó ${dibujoExistente.cantidad} dibujo/s de "${dibujo}" a su carrito`); //se muestra un alert donde incique al usuario que ha agregado un articulo al carrito
+      } else { //si el articulo no existe en el carrito
+            let  nuevoDibujo = productos.find((el) => el.nombre === dibujo); //se declara un objeto nuevo que sea el  del producto elegido
+            carritoStorage.push(nuevoDibujo); //se agrega el producto al carrito que va directo al localStorage
+            alert(`Usted agregó ${nuevoDibujo.cantidad} dibujo/s de "${dibujo}" a su carrito`); //alert que avisa que el usuario añadio al carrito en producto
       }
-}
+
+      localStorage.setItem("carrito", JSON.stringify(carritoStorage)); //subida al localStorage del carrito
+};
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-let mainCatalogo = document.getElementById("mainCatalogo");
+let mainCatalogo = document.getElementById("mainCatalogo"); //obtengo como etiqueta padre el main del index.html
 
-let boton = mainCatalogo.querySelectorAll("button");
+let boton = mainCatalogo.querySelectorAll("button"); //obtengo todos los tag button dentro del main
 
-for (let i of boton) {
+for (let i of boton) { //agrego a cada boton dentro del main un evento del tipo click, y cuya funcion sea agregarCarrito
       i.addEventListener("click", agregarCarrito);
 }
+
+
